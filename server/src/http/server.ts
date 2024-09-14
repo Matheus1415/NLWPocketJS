ba89157cb3ebf65687c39818e978d84b0872d7cb
Matheus <mps.web.tech@gmail.com>
2024-09-14 20:43:12 -0300

    Web completed

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,60 +1,26 @@
 import fastify from "fastify";
-import { createGoal } from "../function/create-goal";
-import z from "zod";
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
-import { getWeekPedingGoals } from "../function/get-week-peding-goals";
-import { createGoalCompletion } from "../function/create-goal-completion";
+import { createGoalRoute } from "./router/create-goal";
+import { createCompletionRoute } from "./router/create-completion";
+import { getPendingGoalRoute } from "./router/get-pending-goals";
+import { getWeekSummarRoute } from "./router/get-week-summar";
+import fastifyCors from "@fastify/cors";
 
-// Cria a instância do Fastify com o tipo de provedor Zod
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-// Configura o compilador de validador e serializador
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.get('/pending-goals', async () => {
-  const { pedingGoals } = await getWeekPedingGoals();
-  return pedingGoals
+app.register(fastifyCors, {
+  origin: "*",
+  methods: ["GET", "POST"],
 })
 
-// Define o schema para o endpoint POST
-app.post('/goals', {
-  schema: {
-    body: z.object({
-      title: z.string(),
-      desiredWeeklyFrequency: z.number().int().min(1).max(7),
-    })
-  }
-}, async (request) => {
-  // Desestrutura o corpo da requisição
-  const { title, desiredWeeklyFrequency } = request.body;
-  
-  // Chama a função para criar o goal
-  await createGoal({
-    title,
-    desiredWeeklyFrequency,
-  });
+app.register(createGoalRoute)
+app.register(createCompletionRoute)
+app.register(getPendingGoalRoute)
+app.register(getWeekSummarRoute)
 
-  // Retorna uma resposta de sucesso
-  return { message: 'Goal created successfully!' };
-});
-
-app.post('/completion', {
-  schema: {
-    body: z.object({
-      goalId: z.string()
-    })
-  }
-}, async (request) => {
-  const { goalId } = request.body;
-
-  await createGoalCompletion({
-    goalId
-  });
-
-});
-
-// Inicia o servidor na porta 3333
 app.listen({
   port: 3333,
 }).then(() => {
